69f89b4898a164d7df62988e5a153ff0ab22229a
Arron M <marshall.arron@gmail.com>
2019-04-26 08:32:30 -0700

    Adjust CSS to prevent scrolling

--- starwars/src/components/Characters.js
+++ starwars/src/components/Characters.js
@@ -5,7 +5,6 @@ import './StarWars.css';
 const Characters = (props) => {
   console.log(props);
   return (
-    <div>
       <ul className="Characters">
        {
           props.chars.map((char) => (
@@ -13,7 +12,6 @@ const Characters = (props) => {
           ))
        }
       </ul>
-    </div>
   );
 }
  
